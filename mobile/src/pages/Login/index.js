dc24cb512bb51ae2a1950024178f3e8c9e906424
Yan <yankaiquecosta.yk@gmail.com>
2020-08-08 21:22:25 -0300

    Login page structure

---
+++ mobile/src/pages/Login/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Container, Middle, IconArea, Input, LoginButton, Text, Icon} from './styles';
+import {useNavigation} from '@react-navigation/native';
+
+const LoginScreen = ()=>{
+    const navigation = useNavigation();
+
+    const handleButtonLogin = ()=>{
+        navigation.navigate('Histories');
+    }
+
+    return(
+        <Container>
+            <Middle>
+                <IconArea>
+                    <Icon source={require('../../assets/pumpkin.png')}/>
+                    <Text style={{color:'#566573',fontSize:30}}> | Login</Text>
+                </IconArea>
+                <Input placeholder='Login'/>
+                <Input placeholder='Password'/>
+                <LoginButton onPress={handleButtonLogin}>
+                    <Text>Next</Text>
+                </LoginButton>
+            </Middle>
+        </Container>
+    );
+}
+
+export default LoginScreen;
