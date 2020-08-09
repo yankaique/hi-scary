4261cf46098503f461bb3b3b57c1502addfd5b14
Yan <yankaiquecosta.yk@gmail.com>
2020-08-08 21:23:11 -0300

    Pages created

---
+++ mobile/src/pages/Histories/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView,Text} from 'react-native';
+
+const HistoriesScreen = () =>{
+    return(
+        <SafeAreaView>
+            <Text>Hello World</Text>
+        </SafeAreaView>
+
+    );
+}
+
+export default HistoriesScreen;
