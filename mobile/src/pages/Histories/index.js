9075827dadb635d51d2f707e0ec513e4650080ac
Yan <yankaiquecosta.yk@gmail.com>
2020-08-11 20:36:15 -0300

    Histories structure

--- mobile/src/pages/Histories/index.js
+++ mobile/src/pages/Histories/index.js
@@ -1,11 +1,57 @@
 import React from 'react';
-import {SafeAreaView,Text} from 'react-native';
+import {Container,
+        MenuButton,
+        Menu,
+        Text,
+        Title,
+        ItemsButton,
+        ItemMenu,
+        MenuArea,
+        Icon,
+        InfoArea,
+        TitleItem} from './styles';
+import items from './data';
+import menuItems from './menuData';
 
 const HistoriesScreen = () =>{
+    var i = 0;
     return(
-        <SafeAreaView>
-            <Text>Hello World</Text>
-        </SafeAreaView>
+        <Container>
+            <Title>Histories</Title>
+            <Menu 
+                data={menuItems}
+                renderItem={({item})=>(
+                        <MenuButton>
+                            <Text>
+                                {item.title}
+                            </Text>
+                        </MenuButton>
+                )}
+                keyExtractor={item => item.id}
+                showsHorizontalScrollIndicator={false}
+                horizontal={true}
+            />
+            <MenuArea>
+                <ItemMenu 
+                    data={items}
+                    renderItem={({item})=>(
+                            <ItemsButton>
+                                <InfoArea style={{flexDirection:'row'}}>
+                                    <Icon source={require('../../assets/pumpkin.png')}/>
+                                    <InfoArea style={{justifyContent:'center',marginLeft:5}}>
+                                        <TitleItem>{item.title}</TitleItem>
+                                        <Text>{item.text}</Text>
+                                    </InfoArea>
+                                </InfoArea>
+                            </ItemsButton>
+                    )}
+                    keyExtractor={item => item.id}
+                    showsVerticalScrollIndicator={false}
+                />
+            </MenuArea>
+
+            
+        </Container>
 
     );
 }
