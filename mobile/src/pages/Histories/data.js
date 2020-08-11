2f2ec748a0e8a148e1fd4ee6c1e271dac34da299
Yan <yankaiquecosta.yk@gmail.com>
2020-08-11 20:35:44 -0300

    Eaxmple data

---
+++ mobile/src/pages/Histories/data.js
@@ -0,0 +1,13 @@
+const items = [
+    {id:'1', title:'A warewolf', text:'In my rom in the night...'},
+    {id:'2', title:'A bruxa do 71', text:'Uma vez na roça dos meus tios apareceu uma bruxa assustadora...' },
+    {id:'3', title:'ASDASIHU', text:'daskljdshndnad'},
+    {id:'4', title:'DSAAS', text:'sdhasbhdasbh'},
+    {id:'5', title:'asd', text:'sdas ndbnasdbas'},
+    {id:'6', title:'dfdg', text:'dsanbdashbdas'},
+    {id:'7', title:'fggert', text:'qwewrtvbv bcvbc'},
+    {id:'8', title:'dasdzxc', text:'wewqet y jyutyyy'},
+    {id:'9', title:'bnb', text:'e1e12 trtr bvb'},
+]
+
+export default items;
---
+++ mobile/src/pages/Histories/menuData.js
@@ -0,0 +1,9 @@
+const menuItems = [
+    {id:'1', title:'Witch'},
+    {id:'2', title:'Werewolf' },
+    {id:'3', title:'Ghost'},
+    {id:'4', title:'Sinister'},
+    {id:'5', title:'Folklore'},
+]
+
+export default menuItems;
