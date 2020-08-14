3410da0ff294504d2503faef2e80e392a63c4224
Yan <yankaiquecosta.yk@gmail.com>
2020-08-14 18:30:08 -0300

    Organized route file

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 
-import HomeScreen from './pages/Home';
-import LoginScreen from './pages/Login';
-import HistoriesScreen from './pages/Histories';
-import RegisterScreen from './pages/Register';
-import HistoryScreen from './pages/History';
-
-const MainStack = createStackNavigator();
+import StackRoute from './navigators/StackNavigator'; 
 
 const Routes = () =>{
     return(
         <NavigationContainer>
-            <MainStack.Navigator screenOptions={{
-                headerShown:false
-            }}>
-                <MainStack.Screen name="Home" component={HomeScreen} options={{
-                    headerShown: false
-                }}/>
-                <MainStack.Screen name="Login" component={LoginScreen} />
-                <MainStack.Screen name="Histories" component={HistoriesScreen} />
-                <MainStack.Screen name="Register" component={RegisterScreen} />
-                <MainStack.Screen name="History" component={HistoryScreen} />
-
-            </MainStack.Navigator>
+            <StackRoute />
         </NavigationContainer>
     );
 }
-
 export default Routes;
