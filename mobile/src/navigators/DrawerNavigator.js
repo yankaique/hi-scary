197e64ac023153685557610f5e365de400dd833f
Yan <yankaiquecosta.yk@gmail.com>
2020-08-16 21:31:03 -0300

    New route PostHistory added to drawer navigator

--- mobile/src/navigators/DrawerNavigator.js
+++ mobile/src/navigators/DrawerNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import HistoriesScreen from '../pages/Histories';
+import PostHistoryScreen from '../pages/PostHistory';
 
 const Drawer = createDrawerNavigator();
 
@@ -9,6 +10,7 @@ const DrawerRoute = ()=>{
     return(
         <Drawer.Navigator initialRouteName="Histories">
             <Drawer.Screen name="Items" component={HistoriesScreen} options={{title:"Histories"}}/>
+            <Drawer.Screen name="PostHistory" component={PostHistoryScreen} options={{title:"Post history"}}/>
         </Drawer.Navigator>
     )
 }
