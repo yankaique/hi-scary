0e0cbfc65f0b04df2d634beb1be959800b76e943
Yan <yankaiquecosta.yk@gmail.com>
2020-08-18 21:23:23 -0300

    New route added

--- mobile/src/navigators/DrawerNavigator.js
+++ mobile/src/navigators/DrawerNavigator.js
@@ -3,14 +3,17 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import HistoriesScreen from '../pages/Histories';
 import PostHistoryScreen from '../pages/PostHistory';
+import MyHistoriesScreen from '../pages/MyHistories';
+
 
 const Drawer = createDrawerNavigator();
 
 const DrawerRoute = ()=>{
     return(
         <Drawer.Navigator initialRouteName="Histories">
-            <Drawer.Screen name="Items" component={HistoriesScreen} options={{title:"Histories"}}/>
             <Drawer.Screen name="PostHistory" component={PostHistoryScreen} options={{title:"Post history"}}/>
+            <Drawer.Screen name="Items" component={HistoriesScreen} options={{title:"Histories"}}/>
+            <Drawer.Screen name="MyHistories" component={MyHistoriesScreen} options={{title:"My Histories"}}/>
         </Drawer.Navigator>
     )
 }
