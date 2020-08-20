41fd569efd371784d2e3de88943914caea588df1
Yan <yankaiquecosta.yk@gmail.com>
2020-08-19 21:03:57 -0300

    Import picker

--- mobile/src/pages/PostHistory/index.js
+++ mobile/src/pages/PostHistory/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Container,Title, HeaderArea, Icon,  InputArea, Input, PostButton, TextButton} from './styles';
+import {Picker} from '@react-native-community/picker';
 import {useNavigation} from '@react-navigation/native';
 
 const PostHistoryScreen = () =>{
+    const [categories, setCategories] = useState('');
+
     const navigation = useNavigation();
 
     const handleHistoriesButton = ()=>{
@@ -18,6 +21,15 @@ const PostHistoryScreen = () =>{
             <InputArea>
                 <Input placeholder="Title..." />
                 <Input placeholder="Subscription..." />
+                <Picker  selectedValue={categories}
+                         style={{height: 50, width: '90%'}} 
+                         onValueChange={(itemValue, itemIndex) =>
+                            setCategories(itemValue)
+                         }
+                >
+                    <Picker.Item label="Werewolf" value="werewolf" />
+                    <Picker.Item label="Ghost" value="ghost" />
+                </Picker>
                 <Input placeholder="History..." multiline={true} numberOfLines={10}/>
                 <PostButton onPress={handleHistoriesButton}>
                     <TextButton>
